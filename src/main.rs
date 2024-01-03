f76de85d06297a11433efd03fa06b10c01a518c9
Robert <robert@oakleyrain.com>
2024-01-02 22:57:09 -0600

    Added to base code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use secp256k1;
 use secp256k1::PublicKey;
 use rand::Rng;
-
-mod signatures;
-mod payment;
+use anyhow::Result;
+//mod signatures;
+//mod payment;
 
 mod wallets;
 
-fn main() {
-    let mut rng = rand::thread_rng();
-    println!("Integer: {}", rng.gen_range(0..10));
-    println!("Float: {}", rng.gen_range(0.0..10.0));
+const URL: &str = "https://eth-ropsten.alchemyapi.io/v2/owrGyNISGOXRtlnncV2XGYZ4a6DvdYi_";
+
+fn main() -> Result<()> {
+    let keypair =wallets::create_keypair();
+    println!("keypair: {:?}", keypair);
+    Ok(())
 }
